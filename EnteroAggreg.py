9a4136256579dadfb1d282e2d9080684a75440f4
Devon Gregory <virologistd@gmail.com>
2022-10-27 14:49:21 -0500

    updating

---
+++ EnteroAggreg.py
@@ -0,0 +1,75 @@
+#!/bin/env python3
+
+import os
+import sys
+
+SAMs = []
+
+for file in os.listdir(os.getcwd()):
+    if (file.lower()).endswith('bt.sam'):
+        SAMs.append(file)
+
+Species_dict_dict = {}
+all_species = {}
+
+if SAMs:
+    for sam in SAMs:
+        print(sam)
+        samp_name = sam[:-4]
+        samp = open(sam, "r")
+        Species_dict_dict[samp_name] = {}
+        total = 0
+        for line in samp:
+            if not line.startswith('@'):
+                splitline = line.strip("\n\r").split("\t")
+                try:
+                    splitline[4]
+                except:
+                    pass
+                else:
+                    # if (splitline[1] == "0" or splitline[1] == "16" or splitline[1] == "4") and len(splitline[9]) == 110:
+                        species = splitline[9] # 'Unmatched'
+                        counts = int(splitline[0].split('-')[-1])
+                        total += counts
+                        matchline = splitline[2] # .split('_')
+                        # if matchline[0] == "*":
+                            # species = 'Unmatched'
+                        # else:
+                            # species = splitline[2]
+                        try:
+                            all_species[species]
+                        except:
+                        
+                            all_species[species] = matchline
+                        else:
+                            if not matchline in all_species[species]:
+                                print(f"different mapping of {species} to {all_species[species]} and {matchline}")
+                                all_species[species] += "+"+matchline
+                        
+                        try:
+                            Species_dict_dict[samp_name][species] += counts
+                        except:
+                            Species_dict_dict[samp_name][species] = counts
+        Species_dict_dict[samp_name]['total'] = total
+        samp.close()
+
+if all_species:
+    rs_fh = open('Enteros.tsv', "w")
+    rs_fh.write(f"Sequence\tmatch")
+    for samps in Species_dict_dict:
+        rs_fh.write(f"\t{samps}({Species_dict_dict[samps]['total']})\t")
+    rs_fh.write("\n")
+    rs_fh.write("\t")
+    for samps in Species_dict_dict:
+        rs_fh.write(f"\tCount\tAbundance")
+    rs_fh.write("\n")
+    sorted_species = sorted(all_species, key=lambda x: all_species[x])
+    for spec in sorted_species:
+        rs_fh.write(f"{spec}\t{all_species[spec]}")
+        for samps in Species_dict_dict:
+            try:
+                rs_fh.write(f"\t{Species_dict_dict[samps][spec]}\t{Species_dict_dict[samps][spec]/Species_dict_dict[samps]['total']}")
+            except:
+                rs_fh.write(f"\t\t")
+
+        rs_fh.write("\n")
--- fasta2tab.py
+++ fasta2tab.py
@@ -19,27 +19,71 @@ args = parser.parse_args()
 name = (args.file.name)
 print(name)
 dict_entries = {}
-for line in args.file:
-    if line.startswith('>'):
-        refID = line[1:].strip("\n\r")
-        try:
-            dict_entries[refID]
-        except:
-            dict_entries[refID] = ''
-        else:
-            i = 1
-            newrefID = refID + '.' + str(i)
-            while newrefID in dict_entries.keys():
-                i += 1
-                newrefID = refID + '.' + str(i)
-            refID =  newrefID
-            dict_entries[refID] = ''
-    else:
-        dict_entries[refID] += line.strip("\n\r")
+with open(name+".tsv","w") as out_fh:
+    with open(name+"2.fa","w") as out2_fh:
+        out_fh.write("ID\tflag\tmulti\tlen\tsequence\n")
+        length = 0
+        ID = ""
+        for line in args.file:
+            if line.startswith('>'):
+                length = int(line.split("len=")[1].split(" ")[0])
+                if length > 1000:
+                    # out_fh.write("\n")
+                    splitline = line.strip().split(" ")
+                    ID = splitline[0].strip(">")
+                    try:
+                        dict_entries[ID]
+                    except:
+                        pass
+                    else:
+                        print(f"Repeat ID {ID}") 
+                        # newID = ""
+                        # repeated_id = True
+                        # x = 1
+                        # while repeated_id:
+                            # newID = ID + "_" + str(x)
+                            # try:
+                                # dict_entries[newID]
+                            # except:
+                                
+                            # else:
+                                # x += 1
+                    dict_entries[ID] = {"seq" : ""}
+                    # out_fh.write(splitline[0].strip(">"))
+                    # out_fh.write("\t")
+                    for segment in splitline[1:]:
+                        dict_entries[ID][segment.split("=")[0]] = segment.split("=")[1]
+                        # out_fh.write(segment.split("=")[1])
+                        # out_fh.write("\t")
+            else:
+                if length > 1000:
+                    dict_entries[ID]["seq"] += line.strip()
+                    # out_fh.write(line.strip())
         
+        sorted_ids = sorted(dict_entries, key=lambda x: int(dict_entries[x]["len"]), reverse=True)
+        for ident in sorted_ids:
+            out_fh.write(f"{ident}\t{dict_entries[ident]['flag']}\t{dict_entries[ident]['multi']}\t{dict_entries[ident]['len']}\t{dict_entries[ident]['seq']}\n")
+            out2_fh.write(f">{ident} flag={dict_entries[ident]['flag']} multi={dict_entries[ident]['multi']} len={dict_entries[ident]['len']}\n{dict_entries[ident]['seq']}\n")
+        
+            # refID = line[1:].strip("\n\r")
+            # try:
+                # dict_entries[refID]
+            # except:
+                # dict_entries[refID] = ''
+            # else:
+                # i = 1
+                # newrefID = refID + '.' + str(i)
+                # while newrefID in dict_entries.keys():
+                    # i += 1
+                    # newrefID = refID + '.' + str(i)
+                # refID =  newrefID
+                # dict_entries[refID] = ''
+        # else:
+            # dict_entries[refID] += line.strip("\n\r")
+            
 
-if dict_entries:
-    outfile = open(name+".tsv","w")
-    for ID in dict_entries:
-        outfile.write(f"{ID}\t{dict_entries[ID]}\n")
-    outfile.close()
+# if dict_entries:
+    # outfile = open(name+".tsv","w")
+    # for ID in dict_entries:
+        # outfile.write(f"{ID}\t{dict_entries[ID]}\n")
+    # outfile.close()
