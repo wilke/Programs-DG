b2e6a29258abfec954dad7ad470e03d313b291ec
Devon Gregory <virologistd@gmail.com>
2024-02-24 17:09:15 -0600

    update

--- compressallthethings.py
+++ compressallthethings.py
@@ -3,8 +3,13 @@
 import os
 import sys
 
-for subdir, dirs, files in os.walk(os.getcwd()):
+base_dir = os.getcwd()
+cur_dir = ""
+for subdir, dirs, files in os.walk(base_dir):
     for file in files:
         if file.endswith('.sam') or file.endswith('.fa') or file.endswith('.fq') or file.endswith('.fasta') or file.endswith('.fastq') or file.endswith('.tsv') or file.endswith('.txt'):
-            if not subdir == os.getcwd():
+            if not subdir == base_dir:
+                if not cur_dir == subdir:
+                    cur_dir = subdir
+                    print(cur_dir)
                 os.system(f"gzip '{os.path.join(subdir, file)}'")
